d2ae7ec4a589e7651b2bca3a1d3967b6c2602d13
AbereTruphenah <145029217+AbereTruphenah@users.noreply.github.com>
2023-10-18 06:56:34 +0300

    Add files via upload

---
+++ book/BookInputTest.java
@@ -0,0 +1,21 @@
+package book;
+import java.util.Scanner;
+public class BookInputTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input=new Scanner(System.in);
+		Book b=new Book();
+		System.out.println("Please enter the title of the book");
+		b.title=input.next();
+		System.out.println("Please enter the author name");
+		b.author=input.next();
+		System.out.println("Please enter the number of pges");
+		b.numberOfPages=input.nextInt();
+		System.out.println("The book title is: "+b.title);
+		System.out.println("The book author is: "+b.author);
+		System.out.println("The book has "+b.numberOfPages+" pages");
+
+	}
+
+}
